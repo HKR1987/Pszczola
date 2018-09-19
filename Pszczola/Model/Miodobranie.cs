ed3fa069670515baf4a3790be55e22be61ec67cb
HKR <hkr1987@gmail.com>
2018-09-19 14:47:45 +0200

    -

---
+++ Pszczola/Model/Miodobranie.cs
@@ -0,0 +1,15 @@
+﻿namespace Pszczola.Model
+{
+    class Miodobranie
+    {
+        public int Id { get; set; }
+        public int IdUl { get; set; }
+        public string Rok { get; set; }
+        public string Data { get; set; }
+        public string Nazwa { get; set; }
+        public int Ramki { get; set; }
+        public double WagaN { get; set; }
+        public double WagaB { get; set; }
+        public string Uwagi { get; set; }
+    }
+}
